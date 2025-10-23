822a8f68e242b6075ce725220ee5edcbf8b17fcc
Heaveil <heaveilly@gmail.com>
2025-10-23 15:25:20 +1100

    Representing Code

--- Tree-Walk_Interpreter/AstPrinter.java
+++ Tree-Walk_Interpreter/AstPrinter.java
@@ -5,8 +5,7 @@ public class AstPrinter implements Expr.Visitor<String> {
 
   @Override
   public String visitBinaryExpr(Expr.Binary expr) {
-    return parenthesize(expr.operator.lexeme,
-                        expr.left, expr.right);
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
   }
 
   @Override
@@ -39,14 +38,11 @@ public class AstPrinter implements Expr.Visitor<String> {
   }
 
   public static void main(String[] args) {
-    Expr expression = new Expr.Binary(
-        new Expr.Unary(
-            new Token(TokenType.MINUS, "-", null, 1),
-            new Expr.Literal(123)),
-        new Token(TokenType.STAR, "*", null, 1),
-        new Expr.Grouping(
-            new Expr.Literal(45.67)));
-
+    Expr expression = new Expr.Binary( new Expr.Unary( 
+        new Token(TokenType.MINUS, "-", null, 1), 
+        new Expr.Literal(123)), 
+        new Token(TokenType.STAR, "*", null, 1), 
+        new Expr.Grouping( new Expr.Literal(45.67)));
     System.out.println(new AstPrinter().print(expression));
   }
 
