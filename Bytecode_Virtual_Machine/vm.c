b5dc4f906a2c9009d789fdbaf17090eabe883f28
Heaveil <heaveilly@gmail.com>
2025-11-03 13:40:40 +1100

    Hash Tables

--- Bytecode_Virtual_Machine/vm.c
+++ Bytecode_Virtual_Machine/vm.c
@@ -29,11 +29,13 @@ static void runtimeError(const char* format, ...) {
 }
 
 void initVM() {
-    resetStack();
-    vm.objects = NULL;
+  resetStack();
+  vm.objects = NULL;
+  initTable(&vm.strings);
 }
 
 void freeVM() {
+  freeTable(&vm.strings);
   freeObjects();
 }
 
